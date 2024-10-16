5f2dabf4aca6590d94a8c1fe48b333bc328034a9
Jacob Fishel <jacobfis8303@gmail.com>
2024-10-16 14:44:03 -0700

    complete

--- lab06-master/main.py
+++ lab06-master/main.py
@@ -1,6 +1,5 @@
 import weather
 
-
 fileName = ''
 weatherDict = {}
 
@@ -17,7 +16,9 @@ while True:
     match str(choice):
         case '1':
             fileName = input("Enter data filename: ")
+            print("Filename: " + fileName )
             weatherDict = weather.read_data(filename = fileName)
+            print(weatherDict)
 
         case '2':
             date = input("Enter date (YYYYMMDD): ")
@@ -27,16 +28,16 @@ while True:
             rain = input("Enter rainfall: ")
             fullDate = str(date) + str(time)
             tempDict = {fullDate: {'t' : temp, 'h': hum, 'r': rain}}
-            weather.write_data(data = tempDict, filename = r'lab06-master\w.dat')
+            weather.write_data(data = tempDict, filename = fileName)
         
         case '3':
             date = input("Enter date (YYYMMDD): ")
-            dailyReport = weather.report_daily(data = weatherDict, date = date)
-            print(dailyReport)
+            r = weather.report_daily(data={"20210203075501": {"t": 55, "h": 87, "r": 0}, "20210203090602": {"t": 63, "h": 84, "r": 0}, "20210203102903": {"t": 71, "h": 79, "r": 0}, "20210203125504": {"t": 72, "h": 69, "r": 0}, "20210203183905": {"t": 59, "h": 75, "r": 0}, "20210205044406": {"t": 57, "h": 68, "r": 0.01}, "20210205083307": {"t": 65, "h": 63, "r": 0.05}, "20210205122208": {"t": 73, "h": 56, "r": 0.11}, "20210205161109": {"t": 74, "h": 60, "r": 0.19}}, date='20210205')
+            # dailyReport = weather.report_daily(data = weatherDict, date = date)
+            print(r)
 
         case '4':
-            historicalReport = weather.report_historical(weatherDict)
-            print(historicalReport)
+            historicalReport = weather.report_historical(data = weatherDict)
 
         case '5':
             break
--- lab06-master/weather.py
+++ lab06-master/weather.py
@@ -3,16 +3,16 @@ from calendar import month_name
 
 def read_data(filename=''):
     try:
-        with open(filename) as f:
-            readData = f.read()
-            return json.dumps(readData)
+        with open(filename, 'r') as f:
+            return json.load(f)
     except FileNotFoundError:
         return {}
+    
 
 def write_data(data={}, filename=''):
-    with open(filename) as f:
-        toJson = json.dumps(data)
-        writeData = f.write(toJson)
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
 
 def max_temperature(data={}, date=''):
     temp = []
@@ -60,56 +60,59 @@ def tot_rain(data={}, date=''):
     return rainSum
 
 def report_daily(data={}, date=''):
-    print("========================= DAILY REPORT========================")
-    print("Date                      Time Temperature  Humidity  Rainfall")
-    print("====================  ======== ===========  ========  ========")
 
     printString = ""
 
+    printString += "========================= DAILY REPORT ========================\n"
+    printString += "Date                      Time  Temperature  Humidity  Rainfall\n"
+    printString += "====================  ========  ===========  ========  ========\n"
+
     for key, value in data.items():
+        if key == None:
+            continue
         if date in key:
             #parsing the date as ints:
             year = key[0:4]
             month = int(key[4:6]) #make it an int to use the month_name func
-            day = key[6:8]
+            day = int(key[6:8])
             hour = key[8:10]
             minute = key[10:12]
             second = key[12:14]
             temp = value['t']
             hum = value['h']
             rain = value['r']
-            # print(month_name[month] + " " + day + ", " + year + "      " + hour.zfill(2) + ":" + minute.zfill(2) + ":" + second.zfill(2))
-            # print(str(temp) + "        " + str(hum) + "        " + str(f"{rain:.2f}"))
+            # printString += month_name[month] + " " + str(day) + ", " + year + "      " + hour.zfill(2) + ":" + minute.zfill(2) + ":" + second.zfill(2) + "           " + str(temp) + "        " + str(hum) + "      " + f"{rain:.2f}" + "\n"
+            printString += f"{month_name[month]} {str(day)}, {year}      {hour.zfill(2)}:{minute.zfill(2)}:{second.zfill(2)}           {str(temp)}        {str(hum)}      {rain:.2f}\n"
 
-            printString += month_name[month] + " " + day + ", " + year + "     " + hour.zfill(2) + ":" + minute.zfill(2) + ":" + second.zfill(2) + "          " + str(temp) + "        " + str(hum) + "      " + str(f"{rain:.2f}" + "\n")
+    # if printString.endswith("\n"):
+    #     return printString[:-1]
     return printString
 
 def report_historical(data = {}):
-    print("============================== HISTORICAL REPORT ===========================")
-    print("	              Minimum      Maximum      Minumum   Maximum   Total")
-    print("Date                  Temperature  Temperature  Humidity  Humidity  Rainfall")
-    print("====================  ===========  ===========  ========  ========  ========")
     
     noRepeats = []
     temp = ""   
+    temp += "============================== HISTORICAL REPORT ===========================\n"
+    temp += "                          Minimum      Maximum   Minumum   Maximum     Total\n"
+    temp += "Date                  Temperature  Temperature  Humidity  Humidity  Rainfall\n"
+    temp += "====================  ===========  ===========  ========  ========  ========\n"
     
     for key, value in data.items():
-        date = key[0:9]
+        date = key[0:8]
         if date in noRepeats:
             continue
         year = date[0:4]
         month = int(date[4:6])
-        day = date[6:8]
+        day = int(date[6:8])
 
         minTemp = min_temperature(data = data, date = date)
-        maxTemp = min_temperature(data = data, date = date)
-        minHum = min_temperature(data = data, date = date)
-        maxHum = min_temperature(data = data, date = date)
+        maxTemp = max_temperature(data = data, date = date)
+        minHum = min_humidity(data = data, date = date)
+        maxHum = max_humidity(data = data, date = date)
         totRain = tot_rain(data = data, date = date)
-        totalString = f"{month_name[month]:<10} {day:<2}, {year:<4}   {minTemp:>7} {maxTemp:>12} {minHum:>10} {maxHum:>9} {totRain:>8}\n"
-        # print(totalString)
+        # totalString = f"{month_name[month]:<10} {str(day):<2}, {year:<4}   {minTemp:>7} {maxTemp:>12} {minHum:>10} {maxHum:>9} {f"{totRain:.2f}":>8}\n"
+        totalString = f"{month_name[month]} {str(day)}, {year}               {minTemp}           {maxTemp}        {minHum}        {maxHum}      {totRain:.2f}\n"
+
         temp += totalString
         noRepeats.append(date)
-
     return temp
-
